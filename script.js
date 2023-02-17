6a9716ef85acce4cfa30e65950098847c998cbdc
RChipman <rschipman@gmail.com>
2023-02-17 15:41:56 -0300

    rainbow promise

--- script.js
+++ script.js
@@ -106,11 +106,39 @@ const fakeRequestPromise = (url) => {
 
 const fakeRequest = (url) => {
   return new Promise((resolve, reject) => {
+    const randomTime = Math.random();
     setTimeout(() => {
-      resolve();
+      if (randomTime < 0.7) {
+        resolve("HERE YOU GO");
+      }
+      reject("BUZZ OFF REJECTED");
     }, 1000);
   });
 };
-fakeRequest("dogs/1").then(() => {
-  console.log("Done with Request");
-});
+fakeRequest("dogs/1")
+  .then((data) => {
+    console.log("Done with Request");
+    console.log("data is:", data);
+  })
+  .catch((err) => {
+    console.log("Oh NOO!", err);
+  });
+
+const delayedColorChange = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
+};
+
+delayedColorChange("red", 1000)
+  .then(() => delayedColorChange("orange", 1000))
+  .then(() => delayedColorChange("yellow", 1000))
+  .then(() => delayedColorChange("green", 1000))
+  .then(() => delayedColorChange("blue", 1000))
+  .then(() => delayedColorChange("indigo", 1000))
+  .then(() => delayedColorChange("violet", 1000))
+  .then(() => delayedColorChange("pink", 1000))
+  .then(() => delayedColorChange("red", 1000));
