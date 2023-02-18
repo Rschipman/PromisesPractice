cbbb1f08f98b97524209ef5e1baeaa32ede43ebd
RChipman <rschipman@gmail.com>
2023-02-18 15:43:06 -0300

    Goooood Morning GITHUB!

--- script.js
+++ script.js
@@ -104,41 +104,45 @@ const fakeRequestPromise = (url) => {
 //   resolve();
 // });
 
-const fakeRequest = (url) => {
-  return new Promise((resolve, reject) => {
-    const randomTime = Math.random();
-    setTimeout(() => {
-      if (randomTime < 0.7) {
-        resolve("HERE YOU GO");
-      }
-      reject("BUZZ OFF REJECTED");
-    }, 1000);
-  });
-};
-fakeRequest("dogs/1")
-  .then((data) => {
-    console.log("Done with Request");
-    console.log("data is:", data);
-  })
-  .catch((err) => {
-    console.log("Oh NOO!", err);
-  });
+// const fakeRequest = (url) => {
+//   return new Promise((resolve, reject) => {
+//     const randomTime = Math.random();
+//     setTimeout(() => {
+//       if (randomTime < 0.7) {
+//         resolve("HERE YOU GO");
+//       }
+//       reject("BUZZ OFF REJECTED");
+//     }, 1000);
+//   });
+// };
+// fakeRequest("dogs/1")
+//   .then((data) => {
+//     console.log("Done with Request");
+//     console.log("data is:", data);
+//   })
+//   .catch((err) => {
+//     console.log("Oh NOO!", err);
+//   });
 
-const delayedColorChange = (color, delay) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      document.body.style.backgroundColor = color;
-      resolve();
-    }, delay);
-  });
-};
+// const delayedColorChange = (color, delay) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = color;
+//       resolve();
+//     }, delay);
+//   });
+// };
 
-delayedColorChange("red", 1000)
-  .then(() => delayedColorChange("orange", 1000))
-  .then(() => delayedColorChange("yellow", 1000))
-  .then(() => delayedColorChange("green", 1000))
-  .then(() => delayedColorChange("blue", 1000))
-  .then(() => delayedColorChange("indigo", 1000))
-  .then(() => delayedColorChange("violet", 1000))
-  .then(() => delayedColorChange("pink", 1000))
-  .then(() => delayedColorChange("red", 1000));
+// delayedColorChange("red", 1000)
+//   .then(() => delayedColorChange("orange", 1000))
+//   .then(() => delayedColorChange("yellow", 1000))
+//   .then(() => delayedColorChange("green", 1000))
+//   .then(() => delayedColorChange("blue", 1000))
+//   .then(() => delayedColorChange("indigo", 1000))
+//   .then(() => delayedColorChange("violet", 1000))
+//   .then(() => delayedColorChange("pink", 1000))
+//   .then(() => delayedColorChange("red", 1000));
+
+async function rainbow() {
+  delayedColorChange("red", 1000);
+}
